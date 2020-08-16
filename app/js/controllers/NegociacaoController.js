574abdcef1fad5acd09fb9843ac746d3cb3b3f4c
Lucas Gomes <lucasgms16@gmail.com>
2020-08-16 20:31:49 -0300

    first commit

---
+++ app/js/controllers/NegociacaoController.js
@@ -0,0 +1,18 @@
+class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.negociacoesView = new NegociacoesView('#negociacoesView');
+        this.mensagemView = new MensagemView('#mensagemView');
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(new Date(this.inputData.value.replace(/-/g, ',')), parseInt(this.inputQuantidade.value), parseFloat(this.inputValor.value));
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso!');
+    }
+}
---
+++ app/ts/controllers/NegociacaoController.ts
@@ -0,0 +1,32 @@
+class NegociacaoController {
+
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView')
+    constructor() {
+        this.inputData = <HTMLInputElement>document.querySelector('#data');
+        this.inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this.inputValor = <HTMLInputElement>document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
+    }
+
+    adiciona(event: Event) {
+
+        event.preventDefault();
+        const negociacao = new Negociacao(
+            new Date(this.inputData.value.replace(/-/g, ',')),
+            parseInt(this.inputQuantidade.value),
+            parseFloat(this.inputValor.value)
+        )
+
+        this.negociacoes.adiciona(negociacao);
+
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso!');
+
+
+    }
+}
